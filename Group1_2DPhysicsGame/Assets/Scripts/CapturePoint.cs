7d6a596a9556328f91d838899ff731a92ecaafae
Matt Lummis <matthew.lummis@gmail.com>
2017-02-08 21:43:58 +0000

    Added my unity project file

---
+++ Group1_2DPhysicsGame/Assets/Scripts/CapturePoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePoint : MonoBehaviour 
+{
+	public Material [] material;
+	Renderer rend;
+
+	//public GameObject BlueZones;
+	//private Score_Display BlueAccess;
+	//private int blueInteger;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+		rend.sharedMaterial = material [0];
+
+	//	BlueAccess = BlueZones.GetComponent<Score_Display> ();
+	//	blueInteger = BlueAccess.blueZoneCounter;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Blue_Team") {
+			print ("Blue Capture");
+			rend.sharedMaterial = material [1];
+		//	blueInteger + 1;
+
+		} 
+
+		if (other.tag == "Red_Team") {
+			print ("Red Capture");
+			rend.sharedMaterial = material [2];
+
+		}
+
+	}
+}
---
+++ Group1_2DPhysicsGame/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw ("Horizontal") > 0) 
+		{
+			transform.Translate (Vector2.right * Time.deltaTime * moveSpeed);	
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0) 
+		{
+			transform.Translate (Vector2.left * Time.deltaTime * moveSpeed);	
+		}
+	}
+}
