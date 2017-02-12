624a679f8469fd32b075e11f7cda1e3f6e04c16e
Matt Lummis <matthew.lummis@gmail.com>
2017-02-12 16:49:07 +0000

    Uploaded my_version of cannon arc test Build / Unity Project.

---
+++ ProjectWork/my_version/Unity Project/Assets/Scripts/barrelRotateScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class barrelRotateScript : MonoBehaviour {
+
+	public int aimSpeed = 5;
+
+	// Use this for initialization
+	void Start () {}
+	
+	void FixedUpdate()
+	{
+       // Horozontal movement 
+		var move = new Vector3(Input.GetAxis("Vertical"), 0, 0);
+		transform.Rotate (Vector3.forward);   
+	}
+}
