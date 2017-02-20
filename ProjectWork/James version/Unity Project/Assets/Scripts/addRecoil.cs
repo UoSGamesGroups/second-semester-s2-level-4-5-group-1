884fe0cbe8041e10654d5acb0fa496ddff2d28c5
Matt Lummis <matthew.lummis@gmail.com>
2017-02-20 17:29:37 +0000

    completed adding shooting mechanic

---
+++ ProjectWork/James version/Unity Project/Assets/Scripts/addRecoil.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addRecoil : MonoBehaviour {
+
+public float recoilForce;				// Thrust applied to projectile
+public Rigidbody2D PCrb;				// Projectile's rigidBody
+public GameObject PC;
+
+	void OnButtonDown()
+		{
+			PCrb = GetComponent<Rigidbody2D> ();
+			PCrb.AddForce (transform.up * recoilForce);       // Adds a single blast of force to the projectile
+		}
+	}
