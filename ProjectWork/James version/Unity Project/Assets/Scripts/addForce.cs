6650e3c466363bf8f3ce508c5ad9f10147f22ac4
JamesPyke <s178316@ucs.ac.uk>
2017-02-20 20:29:54 +0000

    Fixed Projectile clearing

--- ProjectWork/James version/Unity Project/Assets/Scripts/addForce.cs
+++ ProjectWork/James version/Unity Project/Assets/Scripts/addForce.cs
@@ -2,30 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class addForce : MonoBehaviour 
+public class addForce : MonoBehaviour
 {
 
-	public float thrust;				// Thrust applied to projectile
-	public Rigidbody2D rb;				// Projectile's rigidBody
-	public GameObject Projectile;
+    public float thrust;                // Thrust applied to projectile
+    public Rigidbody2D rb;              // Projectile's rigidBody
+    public GameObject Projectile;
 
-	void Start()
-	{
-		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (transform.up * thrust);       // Adds a single blast of force to the projectile
-	}
+    private int boundsHit = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * thrust);       // Adds a single blast of force to the projectile
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Boundry")
+        {
+            boundsHit += 1;
+            if (boundsHit == 3)
+            {
+                bulletHit();
+            }
+        }
+    }
+
+    void bulletHit()
+    {
+        Destroy(this.gameObject);
+    }
 }
 
-/*	void OnCollisionEnter2D(Collision2D col)
-	{
-		if (col.gameObject.tag == "Boundry") 
-		{
-			bulletHit ();
-		}
-	}
-
-	void bulletHit()
-	{
-		Projectile.SetActive (false);
-	}
-*/
